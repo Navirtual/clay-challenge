d2dc220b27166c0c5fcaa15ef8942f5941c6158f
Ivan Araya <ivan@MacBook-Pro-de-Ivan.local>
2023-04-02 23:35:10 -0400

    Feat: Implements production/meteor env on deploy for api path

--- simple-todos-react/imports/ui/services/textos.services.jsx
+++ simple-todos-react/imports/ui/services/textos.services.jsx
@@ -1,5 +1,10 @@
 import { METHOD } from "../utils";
-const BASE_PATH = "http://localhost:3000/api/textos";
+
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://clay-tech-app.meteorapp.com";
+const BASE_PATH = `${BASE_URL}/api/textos`;
 
 export const TextosService = async (lang, componente) => {
   const path = `${BASE_PATH}/${lang}/${componente}`;
